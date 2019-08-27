01681405daacc6384f956e48b45ae428d7aed674
linyang4 <lin.yang4@outlook.com>
2019-08-27 11:22:34 +0800

    feat(mvvm-course): complete task2.3

---
+++ mvvm-course/2.3-styling-control/config/webpack.base.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js&$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.san$/,
+        use: ['babel-loader', 'san-loader']
+      },
+      {
+        test: /\.html$/,
+        use: 'html-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin()]
+};
